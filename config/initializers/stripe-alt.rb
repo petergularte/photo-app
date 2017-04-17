dbbcd1f74660a67c8d533c9909c58eacab66f242
Peter Gularte <petergularte@gmail.com>
2017-04-17 21:09:42 +1000

    Update stripe file to use environment variables

---
+++ config/initializers/stripe-alt.rb
@@ -0,0 +1,8 @@
+Rails.configuration.stripe = {
+
+:publishable_key => Rails.application.secrets.publishable_key,
+:secret_key => Rails.application.secrets.secret_key
+
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,8 @@
 Rails.configuration.stripe = {
 
-:publishable_key => Rails.application.secrets.publishable_key,
-:secret_key => Rails.application.secrets.secret_key
+:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+
+:secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 
 }
 
